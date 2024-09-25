2a88be61ad736f840da7698bb155224cf4f6ad92
Sanjib Adhya <sanjib@zacksresearch.in>
2024-09-25 08:08:05 -0500

    added project

---
+++ src/components/Text.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Text() {
+	return (
+		<>
+			<div className="relative z-10 p-20 mix-blend-difference text-white w-full h-full flex flex-col justify-between">
+				<p className="w-[50vw] text-[2vw] self-end uppercase mix-blend-difference">
+					Beauty and quality need the right time to be conceived and realised
+					even in a world that is in too much of a hurry.
+				</p>
+				<p className="text-[5vw] uppercase mix-blend-difference">
+					Background Parallax
+				</p>
+			</div>
+		</>
+	);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,mdx}",
+    "./pages/**/*.{js,jsx,mdx}",
+    "./components/**/*.{js,jsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,jsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
